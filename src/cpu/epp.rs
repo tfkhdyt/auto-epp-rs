41be9e7a5f66089fe64846c398517a61468af891
tfkhdyt <me@tfkhdyt.my.id>
2023-10-12 06:23:26 +0700

    perf(cpu): don't write to epp and governor file if current epp or governor is still the same

--- src/cpu/epp.rs
+++ src/cpu/epp.rs
@@ -1,15 +1,22 @@
 use std::{path::Path, process};
 
-use crate::utils::fs::write_file;
+use crate::utils::fs::{read_file, write_file};
 
 pub fn set_epp(epp_value: &str) {
     let cpu_count = num_cpus::get();
 
     for cpu in 0..cpu_count {
-        let epp_file_path = format!(
+        let epp_file = format!(
             "/sys/devices/system/cpu/cpu{}/cpufreq/energy_performance_preference",
             cpu
         );
+        let epp_file_path = Path::new(&epp_file);
+
+        if let Ok(current_epp) = read_file(epp_file_path) {
+            if current_epp.trim() == epp_value {
+                continue;
+            }
+        }
 
         if let Err(err) = write_file(Path::new(&epp_file_path), epp_value) {
             eprintln!(
--- src/cpu/governor.rs
+++ src/cpu/governor.rs
@@ -1,17 +1,27 @@
 use std::{path::Path, process};
 
-use crate::utils::fs::write_file;
+use crate::{
+    config,
+    utils::fs::{read_file, write_file},
+};
 
 pub fn set_governor() {
     let num_cores = num_cpus::get();
 
     for cpu in 0..num_cores {
-        let governor_file_path = format!(
+        let governor_file = format!(
             "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_governor",
             cpu
         );
+        let governor_file_path = Path::new(&governor_file);
 
-        if let Err(err) = write_file(Path::new(&governor_file_path), "powersave") {
+        if let Ok(current_governor) = read_file(governor_file_path) {
+            if current_governor.trim() == config::DEFAULT_GOVERNOR {
+                continue;
+            }
+        }
+
+        if let Err(err) = write_file(governor_file_path, config::DEFAULT_GOVERNOR) {
             eprintln!("Error: Failed to create config file: {}", err);
             process::exit(1);
         }
